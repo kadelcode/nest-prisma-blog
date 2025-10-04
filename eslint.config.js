8bf8f990537c8afe9684fce33c1a13e07a9f4aa8
kadelcode <kadelcode@gmail.com>
2025-10-04 23:06:37 +0100

    chore(eslint): update eslint config
    Explicitly import and register plugins

--- eslint.config.js
+++ eslint.config.js
@@ -1,22 +1,24 @@
 import js from "@eslint/js";
 import tseslint from "typescript-eslint";
-import prettier from "eslint-config-prettier";
+import prettierPlugin from "eslint-plugin-prettier";
 import importPlugin from "eslint-plugin-import";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default tseslint.config(
   js.configs.recommended,
   ...tseslint.configs.recommended,
-  prettier,
+  eslintConfigPrettier,
   {
-    plugins: {
-      import: importPlugin,
-    },
     files: ["src/**/*.{ts,js}"],
     languageOptions: {
       parserOptions: {
         ecmaVersion: 2020,
         sourceType: "module",
       },
+    },
+    plugins: {
+      prettier: prettierPlugin,
+      import: importPlugin,
     },
     rules: {
       "@typescript-eslint/explicit-module-boundary-types": "off",
