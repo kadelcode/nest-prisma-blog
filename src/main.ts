ff1d0fa68f371595c18deee4e8ad860432c5e352
kadelcode <kadelcode@gmail.com>
2025-10-04 22:16:29 +0100

    feat(app setup): Initial setup of app to test server
    Add core files in NestJS app (bootstrap + core application module)

---
+++ src/main.ts
@@ -0,0 +1,13 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+
+async function bootstrap() {
+    const app = await NestFactory.create(AppModule);
+
+    app.setGlobalPrefix('v1/api');
+    await app.listen(8000);
+    console.log('Application is running on: http://localhost:8000/v1/api');
+}
+
+bootstrap();
