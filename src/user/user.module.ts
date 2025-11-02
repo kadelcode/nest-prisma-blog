70d1c953a9cf7521f6440c7edbe61bb5d99b02c2
kadelcode <kadelcode@gmail.com>
2025-11-02 15:31:39 +0100

    fix(prisma): UnknownDependenciesException error fix
    This setup is done to make Prisma available across the entire application

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { PrismModule } from 'prisma/prisma.module';
 
 import { UserController } from './user.controller';
 import { UserService } from './user.service';
 
 @Module({
+  imports: [PrismModule],
   providers: [UserService],
   controllers: [UserController],
 })
